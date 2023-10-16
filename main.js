d5ecc9f039abe3639538cdff0243f151c7437806
lewis machabe <lewismachabe@gmail.com>
2023-10-16 07:50:14 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// I have converted the string "45" into a numeric value using the plus operator
+
+const number = “45”;
+//This will diplay numeric 45 in the console.
+
+console.log(+number);
+
+
+// First i will convert the string "20" to a number  and then add it to 40
+const printSum = Number("20") + 40; 
+console.log(printSum);// This will print result as 60.
+
+
+// I have Created a sentence by combining strings and the sum of 23 and 4
+const sentence = "I am " + (23 + 4) + " years old"; 
+console.log(sentence);// The result will be "I am 27 years old".
