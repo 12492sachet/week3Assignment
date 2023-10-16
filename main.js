e673960b501aa77f1d79e4956a18aaa21258e953
lewis machabe <lewismachabe@gmail.com>
2023-10-16 08:06:21 +0300

    Removing bags

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // I have converted the string "45" into a numeric value using the plus operator
-
 const number = “45”;
-//This will diplay numeric 45 in the console.
 
+//This will diplay numeric 45 in the console.
 console.log(+number);
 
 
@@ -14,3 +13,10 @@ console.log(printSum);// This will print result as 60.
 // I have Created a sentence by combining strings and the sum of 23 and 4
 const sentence = "I am " + (23 + 4) + " years old"; 
 console.log(sentence);// The result will be "I am 27 years old".
+
+//You cannot change a variable declared using const. this means i will declare it using let instead of const
+let myVaue = “May”;
+
+
+myValue = 86;// this is now allowed no errors
+
